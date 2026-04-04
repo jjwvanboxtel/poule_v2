ef3e7a3a42ec59f1ae18dd6342665aa00030aea1
jjwvanboxtel <jjw.van.boxtel@gmail.com>
2026-04-04 22:22:34 +0200

    Remove whitespace

--- modules/usergroups/usergroup.tpl.php
+++ modules/usergroups/usergroup.tpl.php
@@ -12,7 +12,6 @@
 <div id="usergroup" class="card card-body">
     <div class="table-responsive">
         <table class="list" cellpadding="0" cellspacing="0">
-
             <tr>
                 <th style="width: 20px;"></th>
                 <th style="width: 40px;">{LANG_ID}</th>
