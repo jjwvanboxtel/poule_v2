5701fe79fe2b944ea2f010a0e9cbe4a833a62b79
jjwvanboxtel <jjw.van.boxtel@gmail.com>
2026-04-04 22:16:27 +0200

    improve lists

--- modules/usergroups/usergroup.tpl.php
+++ modules/usergroups/usergroup.tpl.php
@@ -3,20 +3,25 @@
 <div class="title">
     <h2>{COM_NAME}</h2>
 </div>
+
 {USERGROUP_MSG}
-{LINK_ADD}<br />
+<br />
+{LINK_ADD}
+<br />
 
 <div id="usergroup" class="card card-body">
-    <table class="list" cellpadding="0" cellspacing="0">
+    <div class="table-responsive">
+        <table class="list" cellpadding="0" cellspacing="0">
 
-        <tr>
-            <th style="width: 20px;"></th>
-            <th style="width: 40px;">{LANG_ID}</th>
-            <th>{LANG_USERGROUP_NAME}</th>
-            <th style="width: 110px;">{LANG_MEMBERCOUNT}</th>
-            {ACTIONS}
-        </tr>
+            <tr>
+                <th style="width: 20px;"></th>
+                <th style="width: 40px;">{LANG_ID}</th>
+                <th>{LANG_USERGROUP_NAME}</th>
+                <th style="width: 110px;">{LANG_MEMBERCOUNT}</th>
+                {ACTIONS}
+            </tr>
 
-        {CONTENT}
-    </table>
+            {CONTENT}
+        </table>
+    </div>
 </div>
